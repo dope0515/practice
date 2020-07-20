247855e9b864bd4f698de50c337e4c8ee3838dff
dope0515 <61546301+dope0515@users.noreply.github.com>
2020-07-20 18:46:59 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+
+<?php
+#php 시작 
+
+echo "Hello World"; //echo는 js의 docment.write()와 비슷한 개념이다.
+
+#php 끝
+
+?>
+
+</body>
+</html>
